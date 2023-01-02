08737e79f1d4ff08ab161943b0f3b67a064551f3
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-02 16:01:51 +0100

    TVA en utilisant une valeur const

--- class/poo_chiny/calculTVA/index.php
+++ class/poo_chiny/calculTVA/index.php
@@ -1,9 +1,10 @@
 <?php
     class Facture {
+    const TVA = 20;
         public function monantTTC($ht){
-        return $ht * (1 + (20 / 100));
+        return $ht * (1 + (Facture::TVA / 100));
         }
     }
 $facteur = new Facture();
 
-echo $facteur->monantTTC(300);
+echo $facteur->monantTTC(500);
